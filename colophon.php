494cbdb22ed371281bde2a565075d8423cece4ac
Ian Patrick Hines <me@ianpatrickhines.com>
2012-10-19 14:37:49 -0500

    Added "Branch This!" Button

--- colophon.php
+++ colophon.php
@@ -14,28 +14,32 @@ get_header(); ?>
 <div class="eight columns offset-by-two archive" id="content">
 	<article class="article permalink" id="about">
         <header>       
-		<h2>About Ian</h2>
+		<h2>Colophon</h2>
         </header>
         
-        <p>Hello, my name is Ian Patrick Hines. Thanks for stopping by.</p>
-        
         <p><img src="http://d.pr/i/4kCi+" alt="Photo: Ian Patrick Hines" /></p>
+        
+        <p>My name is Ian Patrick Hines, and I live in Cypress, Texas with my wife Christy and our two children, Oliver &amp; Moira. </p>              
 		
-		<p>I’m a pretty lucky guy. I have a beautiful wife, named Christy, and two wonderful children. I have the good fortune of loving what I do. I have great parents, an awesome sister, and fantastic friends. My life has been good to me; I really can’t complain.</p>
+		<p>I've been writing an eponymous weblog since 2009, and I am very interested in character, excellence and how the principles of design can be applied in pursuit of the greater good.</p>
+
+		<p>More than anything else, I love to pursue elegant solutions to complex problems within a given set of constraints and with limited resources.</p>		
 		
-		<p>I believe in the Holy Trinity. I believe the Divine Son became Man, that He died for our sins, and that He will come again to judge the living and the dead. I believe that these things, and all the teachings of the Holy Catholic Church, were revealed by God—who can neither deceive nor be deceived.</p>
+		<p>You can follow me on Twitter, <a href="https://twitter.com/intent/user?screen_name=ianpatrickhines">here</a>.</p>
 		
-		<p>I’m a bit of a political junkie. I believe that Public Service is an honor and a privilege. I believe that the measure of a persons life is the good that they have done, not the wealth they have accumulated. I believe that you must be the change that you wish to see in the world, and I try my hardest to act accordingly. I wear a ReminderBand with two phrases on it: (1) Life is about Focus & (2) Dream Big Dreams. I try to live by these phrases.</p>
+		<h2>The Site</h2>
 		
-		<p>I’m a brother in Alpha Sigma Phi fraternity. I like to write. I dabble in photography. I have an unusual love for hazelnut coffee, cheese danishes, and British teas. I love music, and have amassed a collection of over 10,600 songs. I enjoy exploring new technology. I love the web. I enjoy a good conversation, and love to meet new people.</p>
+		<p>This site is built upon the latest version of <a href="http://wordpress.org" title="Link to WordPress.org">WordPress</a>, coded in <a href="http://www.panic.com/coda/" title="Link to Coda by Panic">Coda</a>, and hosted on a <a href="http://asmallorange.com" title="Learn more about A Small Orange">A Small Orange</a>. All entries are written in <a href="http://www.red-sweater.com/marsedit/" title="Link to MarsEdit by Red Sweater">MarsEdit</a>.</p>
 	</article>
 	
     <article class="article permalink" id="colophon">
         <header>
-            <h2>The Site</h2>
+            <h2>Contact</h2>
         </header>
 
-        <p>This site is built upon the latest version of <a href="http://wordpress.org" title="Link to WordPress.org">WordPress</a>, coded in <a href="http://www.panic.com/coda/" title="Link to Coda by Panic">Coda</a>, and hosted on a <a href="http://asmallorange.com" title="Learn more about A Small Orange">A Small Orange</a>. All entries are written in <a href="http://www.red-sweater.com/marsedit/" title="Link to MarsEdit by Red Sweater">MarsEdit</a>.</p>
+        <p>Please, feel free to contact me on <a href="http://twitter.com/intent/tweet?text=@ianpatrickhines">Twitter</a> or by email at <a href="mailto:hello@ianpatrickhines.com">hello@ianpatrickhines.com</a>.</p>
+        
+        <p>I respond to every email I receive, though it may take some time. Please be patient and know that I really do appreciate that you took the time to write.</p>
 	</article>
 	
 	<article class="article permalink" id="heading">
@@ -59,11 +63,11 @@ get_header(); ?>
             <div class="linked">                
                 <article class="link linked row indexPage" id="subscribe">
                     <header class="one column alpha">
-                        <p><span class="permalink"><a href="https://twitter.com/intent/user?screen_name=ianpatrickhines" class="ss-icon ss-social">twitter</a></span></p>
+                        <p><span class="permalink"><a href="https://twitter.com/intent/user?screen_name=byianhines" class="ss-icon ss-social">twitter</a></span></p>
                     </header>
 
                     <div class="seven columns omega">                        
-                        <h2><a href="https://twitter.com/intent/user?screen_name=ianpatrickhines" title="Follow @ianpatrickhines on Twitter">Follow on Twitter</a></h2>
+                        <h2><a href="https://twitter.com/intent/user?screen_name=byianhines" title="Follow @byianhines on Twitter">Follow on Twitter</a></h2>
                     </div>
                 </article>
             </div> 
--- sidebar.php
+++ sidebar.php
@@ -14,14 +14,18 @@
                     <p class="byline">Do Good Through Design</p>
 
                     <nav role="navigation">
-                        <ul>
-                        	<li id="nav-colophon"><a href="/wrote-a-colophon">About</a></li>
-                        
-                            <li id="nav-archive"><a href="/archives">Archives</a></li>                                                       
+                        <ul>                        
+							<li id="nav-archive"><a href="/archives">Previous Articles</a></li>
+
+                        	<li id="nav-rss"><a href="/wrote-a-colophon">About</a></li>                                                                                                                                  
+							
+                        	<li id="nav-archive"><a href="/wrote-a-colophon">About &amp; Contact</a></li>                                                                                                                                  
+                            
+                            <li id="nav-archive"><a href="https://twitter.com/intent/user?screen_name=ianpatrickhines">@ianpatrickhines</a></li>
 
                             <li id="nav-rss"><a href="/syndicates">Subscribe</a></li>
 
-                            <li id="nav-social"><a href="https://twitter.com/intent/user?screen_name=byianhines" class="ss-icon ss-social" style="color: #4099FF; font-weight:bold; font-size:24px" target="_blank">twitter</a> <a href="http://feeds.feedburner.com/ianpatrickhines" class="ss-icon ss-social" style="color: #f08e2d; font-weight:bold; font-size:24px">rss</a></li>
+                            <li id="nav-social"><a href="https://www.facebook.com/pages/ianhines/289741151126219" class="ss-icon ss-social" style="color: #3B5998; font-weight:bold; font-size:24px" target="_blank">facebook</a> <a href="https://twitter.com/intent/user?screen_name=byianhines" class="ss-icon ss-social" style="color: #4099FF; font-weight:bold; font-size:24px" target="_blank">twitter</a> <a href="http://feeds.feedburner.com/ianpatrickhines" class="ss-icon ss-social" style="color: #f08e2d; font-weight:bold; font-size:24px">rss</a></li>
                         </ul>
                     </nav>
 	                <div id="fusion_ad">
